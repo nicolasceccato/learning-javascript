317c8a65e774ba43a4421c5e18ce44b413a6cfae
nicolasceccato <nicolasceccato@hotmail.com>
2022-10-25 17:56:26 -0300

    Alguns cuidados necessários com o tipo number

---
+++ Fundamentos/cuidadosNumber.js
@@ -0,0 +1,20 @@
+console.log(7/0); // tende ao infinito;
+
+console.log("10"/2);
+
+console.log("texto"*2);
+
+console.log(0.2 + 0.4);
+console.log(0.1 + 0.7);
+
+//console.log(10.toString); nao funciona c o literal
+console.log((10.55555).toString());
+const a = 10
+console.log(typeof a);
+
+console.log(a.toString());
+console.log(typeof a);//n converteu a
+
+console.log(typeof (a.toString()));
+
+console.log((10.464891651.toFixed(1)));
