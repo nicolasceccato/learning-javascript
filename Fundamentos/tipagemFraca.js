a241d52b00f29b5aad9e64a8d38cb2aabd09b790
nicolasceccato <nicolasceccato@hotmail.com>
2022-10-25 17:33:22 -0300

    Exemplo da tipagem fraca de JavaScript

---
+++ Fundamentos/tipagemFraca.js
@@ -0,0 +1,16 @@
+let qualquer = 'exemplo';
+console.log(qualquer);
+console.log(typeof qualquer);
+
+qualquer = 3;
+console.log(qualquer);
+console.log(typeof qualquer);
+
+qualquer = true;
+console.log(qualquer);
+console.log(typeof qualquer);
+
+qualquer = Math.PI;
+console.log(qualquer);
+console.log(typeof qualquer);
+
