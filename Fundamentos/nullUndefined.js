5768dcc2754d91c99c456bb313f32a831cd2ea36
nicolasceccato <nicolasceccato@hotmail.com>
2022-10-31 21:14:54 -0300

    exemplo de null e undefined

---
+++ Fundamentos/nullUndefined.js
@@ -0,0 +1,13 @@
+let valor;
+console.log(valor); //n inicializada
+valor = null; // n aponta para nenhum local de memoria
+console.log(valor); 
+let x = 5;
+
+const produto = {};
+console.log(produto.preco);
+console.log(!!produto.preco);
+
+produto.preco = 3.5;
+console.log(produto);
+console.log(!!produto.preco);
