5ca5556b6797bb5180d5d6f3596100a54526ae17
nicolasceccato <nicolasceccato@hotmail.com>
2022-11-01 21:28:04 -0300

    Exemplo de utilizacao da funcao arrow e armazenando uma funcao em uma constante

---
+++ Fundamentos/funcao2.js
@@ -0,0 +1,21 @@
+//armazenando uma funcao em uma constante
+const imprimirSoma = function(a = 0,b = 0){
+    console.log(a+b);
+}
+imprimirSoma()
+
+//armazenando funcao arrow em uma variavel
+
+const soma = (a = 0,b = 0) => {
+    return a + b;
+}
+console.log(soma(1,2));
+imprimirSoma(soma(2,5));
+
+//retorno implicito
+const subtracao = (a,b) => a - b;
+console.log(subtracao(2,3));
+
+const imprimir2 = a => console.log(a);
+
+imprimir2("boa noite");
