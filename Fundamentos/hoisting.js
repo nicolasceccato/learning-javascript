4d3f2e5f699b240993bac9928dfc1067d5d7b491
nicolasceccato <nicolasceccato@hotmail.com>
2022-11-03 22:29:29 -0300

    Verificando hoisting com variaveis declaradas com var

---
+++ Fundamentos/hoisting.js
@@ -0,0 +1,23 @@
+console.log("a = ", a);
+var a = 2;
+console.log("a = ", a);
+
+function teste(){
+    console.log("b = ", b);
+    var b = 5;
+    console.log("b = ", b);
+}
+teste();
+
+console.log("c = ", c);
+let c = 7;
+console.log("c = ", c);
+
+function teste2(){
+    
+    console.log("d = ", d);
+    let d = 7;
+    console.log("d = ", d);
+
+}
+teste2();
