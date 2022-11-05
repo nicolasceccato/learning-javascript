b74d30669e5109eabbec6838004e3cf37574d54e
nicolasceccato <nicolasceccato@hotmail.com>
2022-11-04 21:01:35 -0300

    Relação entre nome da variavel e seu valor e seu contexto

---
+++ Fundamentos/parNomeValor.js
@@ -0,0 +1,22 @@
+const saudacao = "opa";
+
+function exerc(){
+    const saudacao = "OLá";
+    return saudacao;
+}
+console.log(saudacao);
+
+const Cliente = {
+    nome: "Pedro",
+    idade: 32,
+    peso: 90,
+    endereco: {
+        logradouro: "Rua tal",
+        numero:123.
+    }
+}
+console.log(exerc());
+console.log(Cliente);
+var x = 1
+var x = 2
+console.log(x)
